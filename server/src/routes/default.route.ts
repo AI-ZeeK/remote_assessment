3985b81856bc0206acbf49d6a077f81a22582199
Isaac Williams <Isaaciyaye174@gmail.com>
2024-08-29 01:22:28 +0100

    Update default.route.ts

--- server/src/routes/default.route.ts
+++ server/src/routes/default.route.ts
@@ -1,20 +1,10 @@
-import { Request, Response, Router } from 'express';
+import { Router } from 'express';
 import { Routes } from '@interfaces/routes.interface';
-import multer from 'multer';
 
 export class DefaultRoute implements Routes {
   public path = '/';
   public router = Router();
 
-  storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, 'uploads/');
-    },
-    filename: function (req, file, cb) {
-      cb(null, `${Date.now()}_${file.originalname}`);
-    },
-  });
-  upload = multer({ storage: this.storage });
   constructor() {
     this.initializeRoutes();
   }
@@ -22,14 +12,6 @@ export class DefaultRoute implements Routes {
   private initializeRoutes() {
     this.router.get(`${this.path}`, (req, res) => {
       res.status(200).json('HELLO!!!..., YOU FOUND ME');
-    });
-    this.router.post(`${this.path}api/uploadfile`, this.upload.single('file'), (req: Request & { file: any }, res: Response, next) => {
-      try {
-        const file = req.file ? req.file.filename : null;
-        res.status(201).json({ data: `${process.env.API_URL}/uploads/${file}`, message: 'file uploaded successfully' });
-      } catch (error) {
-        next(error);
-      }
     });
   }
 }
